56519ac6de098b6522b20508eb6a7d2793a423e3
AutoTechWebDev <150986379+AutoTechWebDev@users.noreply.github.com>
2023-11-27 19:53:35 -0500

    Create Sample-portfolio.js
    Sample-Portfolio Javascript

---
+++ Sample-portfolio.js
@@ -0,0 +1,16 @@
+# Sample Portfolio Javascript
+# by James Williamson  
+# 11/27/23
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
